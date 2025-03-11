1e8e6d4864027f2e73694e567f005743bcafea07
Hannes-Walden <hannes.walden@outlook.com>
2025-03-11 10:40:04 +0200

    Add code which creates web api (not complete yet)

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+public class Program
+{
+
+    // VSCODE COMMAND PALETTE!!!
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting!");
+
+        // First we need a web service builder.
+        // (factory design pattern..)
+        // CreateBuilder creates a builder (a factory)
+
+        var builder = WebApplication.CreateBuilder();
+
+        // builder.Build creates a web app
+        var webapi = builder.Build();
+
+        // after that we can use the builder to create the web api.
+
+        // once we have the web api we can start it.
+    }
+}
